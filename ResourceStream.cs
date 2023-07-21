7235afb357ab966082e2d36197a028c83949c3b8
YPazharski <136801415+YPazharski@users.noreply.github.com>
2023-07-21 13:03:40 +0300

    Добавьте файлы проекта.

---
+++ ResourceStream.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams.Resources
+{
+    public class ResourceReaderStream : Stream
+    {
+        public ResourceReaderStream(Stream stream, string key)
+        {
+            // You should not use stream in the constructor of wrapping stream.
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            // if key not found yet: SeekValue();
+            // if value is not read yet: ReadFieldValue(...)
+            // else return 0;
+        }
+
+        private void SeekValue()
+        {
+            // while not end of stream read next section key, compare with required key and skip value if read key is not equal to required key
+        }
+
+        public override void Flush()
+        {
+            // nothing to do
+        }
+    }
+}
